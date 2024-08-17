38520bf9c1b54121d2560aac737dcdef63e29273
dariocolaciuri <darioguitarras@gmail.com>
2024-08-17 13:23:55 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img className='logo' src="logo2.png" alt="logo" />
+      <h3>Proximamente...</h3>
+      <div className='redes'>
+        <a className='spotify' href=""><i className="bi bi-spotify"></i></a>
+        <a className='instagram' href=""><i className="bi bi-instagram"></i></a>
+      </div>
+    </>
+  )
+}
+
+export default App
