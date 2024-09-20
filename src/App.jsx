73a4433f6ca1266563b630e72086befe28700b6e
Dario Colaciuri <darioguitarras@gmail.com>
2024-09-20 19:32:57 -0300

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <img className='logo' src="logo2.png" alt="logo" />
-      <h3>Proximamente...</h3>
+      <h3>Pronto...</h3>
       <div className='redes'>
         <a className='spotify' href=""><i className="bi bi-spotify"></i></a>
         <a className='instagram' href=""><i className="bi bi-instagram"></i></a>
